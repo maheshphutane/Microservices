d4387745becab5918a6fbbcd58e03af4e4af494f
maheshdphutane <maheshphutane1810@gmail.com>
2023-08-27 11:58:33 +0530

    Added CircuitBreaker in API Gateway

--- CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/example/CloudGateway/CloudGatewayApplication.java
@@ -1,7 +1,14 @@
 package com.example.CloudGateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+
+import static ch.qos.logback.classic.spi.ThrowableProxyVO.build;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
@@ -10,4 +17,13 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(
+				id -> new Resilience4JConfigBuilder(id)
+						.circuitBreakerConfig(
+								CircuitBreakerConfig.ofDefaults()
+						).build()
+		);
+	}
 }
